c63f8eae335ff1e17444d128963ed3cf4e3d1050
Keijiro Takahashi <keijiro@gmail.com>
2024-05-24 16:46:44 +0900

    Cap

--- Assets/Scripts/SweepBuilder.cs
+++ Assets/Scripts/SweepBuilder.cs
@@ -19,15 +19,16 @@ public struct SweepConfig
     public float2 SweepAngle;
     public float2 Radius;
     public float2 Width;
-    public float Depth;
+    public float2 Height;
+    public float Displace;
     public uint Seed;
 
     #endregion
 
     #region Helper properties
 
-    public int VertexPerInstance => (Subdivision + 1) * 2;
-    public int IndexPerInstance => Subdivision * 6;
+    public int VertexPerInstance => (Subdivision + 1) * 8 + 8;
+    public int IndexPerInstance => Subdivision * 6 * 4 + 12;
 
     public int TotalVertexCount => InstanceCount * VertexPerInstance;
     public int TotalIndexCount => InstanceCount * IndexPerInstance;
@@ -35,7 +36,7 @@ public struct SweepConfig
     public Bounds BoundingBox
       => new Bounds(Vector3.zero,
                     math.float3(1, 1, 0) * Radius.y +
-                    math.float3(0, 0, 1) * Depth);
+                    math.float3(0, 0, 1) * (Height.y + Displace));
 
     #endregion
 
@@ -49,7 +50,8 @@ public struct SweepConfig
              SweepAngle = math.float2(0.3f, 1.0f),
              Radius = math.float2(0.2f, 0.8f),
              Width = math.float2(0.1f, 0.2f),
-             Depth = 0.1f,
+             Height = math.float2(0.01f, 0.02f),
+             Displace = 0.1f,
              Seed = 0xdeadbeef };
 
     #endregion
@@ -80,21 +82,63 @@ public struct SweepBuilderVertexJob : IJobParallelFor
         var width = R.RangeXY(C.Width) / 2;
         var angle = R.RangeXY(C.BaseAngle);
         var sweep = R.RangeXY(C.SweepAngle);
-        var z = R.SNorm() * C.Depth;
+        var height = R.RangeXY(C.Height) / 2;
+        var disp = R.SNorm() * C.Displace;
+        var z1 = disp - height;
+        var z2 = disp + height;
 
-        var normal = math.float3(0, 0, -1);
+        var zn = math.float3(0, 0, -1);
+        var zp = math.float3(0, 0, +1);
+
         var rsub = 1.0f / C.Subdivision;
 
         var wp = index * C.VertexPerInstance;
+
+        float3 ArcPoint(float t)
+          => math.float3(math.cos(t), math.sin(t), 0);
+
+        (float3, float3, float3, float3) PointToSlice(float3 n)
+          => (math.float3(n.xy * (radius - width), z1),
+              math.float3(n.xy * (radius + width), z1),
+              math.float3(n.xy * (radius - width), z2),
+              math.float3(n.xy * (radius + width), z2));
+
+        {
+            var point = ArcPoint(angle - 0.5f * sweep);
+            var slice = PointToSlice(point);
+            var normal = math.cross(point, math.float3(0, 0, 1));
+            Output[wp++] = new Vertex(slice.Item1, normal);
+            Output[wp++] = new Vertex(slice.Item2, normal);
+            Output[wp++] = new Vertex(slice.Item3, normal);
+            Output[wp++] = new Vertex(slice.Item4, normal);
+        }
+
         for (var i = 0; i <= C.Subdivision; i++)
         {
-            var param = i * rsub;
-            var theta = angle + sweep * (param - 0.5f);
-            var xy = math.float2(math.cos(theta), math.sin(theta));
-            var p1 = math.float3(xy * (radius - width), z);
-            var p2 = math.float3(xy * (radius + width), z);
-            Output[wp++] = new Vertex(p1, normal);
-            Output[wp++] = new Vertex(p2, normal);
+            var n = ArcPoint(angle + sweep * (i * rsub - 0.5f));
+            var slice = PointToSlice(n);
+
+            Output[wp++] = new Vertex(slice.Item1, zn);
+            Output[wp++] = new Vertex(slice.Item2, zn);
+
+            Output[wp++] = new Vertex(slice.Item1, -n);
+            Output[wp++] = new Vertex(slice.Item2, +n);
+
+            Output[wp++] = new Vertex(slice.Item3, zp);
+            Output[wp++] = new Vertex(slice.Item4, zp);
+
+            Output[wp++] = new Vertex(slice.Item3, -n);
+            Output[wp++] = new Vertex(slice.Item4, +n);
+        }
+
+        {
+            var point = ArcPoint(angle + 0.5f * sweep);
+            var slice = PointToSlice(point);
+            var normal = math.cross(point, math.float3(0, 0, -1));
+            Output[wp++] = new Vertex(slice.Item1, normal);
+            Output[wp++] = new Vertex(slice.Item2, normal);
+            Output[wp++] = new Vertex(slice.Item3, normal);
+            Output[wp++] = new Vertex(slice.Item4, normal);
         }
     }
 }
@@ -117,15 +161,55 @@ public struct SweepBuilderIndexJob : IJobParallelFor
     {
         var wp = index * Config.IndexPerInstance;
         var rp = (uint)(index * Config.VertexPerInstance);
-        for (var i = 0u; i < Config.Subdivision; i++, rp += 2)
+
+        Output[wp++] = rp + 0;
+        Output[wp++] = rp + 1;
+        Output[wp++] = rp + 2;
+        Output[wp++] = rp + 1;
+        Output[wp++] = rp + 3;
+        Output[wp++] = rp + 2;
+        rp += 4;
+
+        for (var i = 0u; i < Config.Subdivision; i++, rp += 8)
         {
             Output[wp++] = rp + 0;
-            Output[wp++] = rp + 2;
+            Output[wp++] = rp + 8;
             Output[wp++] = rp + 1;
+            Output[wp++] = rp + 1;
+            Output[wp++] = rp + 8;
+            Output[wp++] = rp + 9;
+
+            Output[wp++] = rp + 4;
+            Output[wp++] = rp + 5;
+            Output[wp++] = rp + 12;
+            Output[wp++] = rp + 5;
+            Output[wp++] = rp + 13;
+            Output[wp++] = rp + 12;
+
             Output[wp++] = rp + 2;
+            Output[wp++] = rp + 6;
+            Output[wp++] = rp + 10;
+            Output[wp++] = rp + 6;
+            Output[wp++] = rp + 14;
+            Output[wp++] = rp + 10;
+
             Output[wp++] = rp + 3;
-            Output[wp++] = rp + 1;
+            Output[wp++] = rp + 11;
+            Output[wp++] = rp + 7;
+            Output[wp++] = rp + 7;
+            Output[wp++] = rp + 11;
+            Output[wp++] = rp + 15;
         }
+
+        rp += 8;
+
+        Output[wp++] = rp + 0;
+        Output[wp++] = rp + 2;
+        Output[wp++] = rp + 1;
+        Output[wp++] = rp + 1;
+        Output[wp++] = rp + 2;
+        Output[wp++] = rp + 3;
+        rp += 4;
     }
 }
 
