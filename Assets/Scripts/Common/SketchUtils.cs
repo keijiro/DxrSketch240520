d9c651d74ffc64ced8c44b9c0808967b21e2db2a
Keijiro Takahashi <keijiro@gmail.com>
2024-06-02 22:30:43 +0900

    Tweaks

--- Assets/Scripts/Common/SketchUtils.cs
+++ Assets/Scripts/Common/SketchUtils.cs
@@ -16,14 +16,49 @@ public static class SketchUtils
     public static NativeArray<T> NewTempJobArray<T>(int count) where T : struct
       => new NativeArray<T>(count, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
 
+    public static AffineTransform AffineTransform(in Transform xform)
+      => new AffineTransform(xform.position, xform.rotation, xform.localScale);
+
+    public static void SafeRelease<T>(ref this NativeArray<T> array) where T : struct
+    {
+        if (array.IsCreated) array.Dispose();
+    }
+
+    public static (float, float) FadeInOut(float life, float fade, float time)
+    {
+        float Pow(float x)
+        {
+            x = x * x;
+            x = x * x;
+            return x * x;
+        }
+        return (1 - Pow(1 - math.saturate(time / fade)),
+                Pow(math.saturate((time - fade - life) / fade)));
+    }
+
     public static float SNorm(ref this Random rand)
       => rand.NextFloat(-0.5f, 0.5f);
 
     public static float UNorm(ref this Random rand)
       => rand.NextFloat(0.0f, 1.0f);
 
+    public static float2 UNorm2(ref this Random rand)
+      => rand.NextFloat2(0.0f, 1.0f);
+
     public static float RangeXY(ref this Random rand, float2 minmax)
-      => math.lerp(minmax.x, minmax.y, rand.UNorm());
+      => rand.NextFloat(minmax.x, minmax.y);
+
+    public static float RangeXYPow(ref this Random rand, float2 minmax, float pow)
+      => math.lerp(minmax.x, minmax.y, math.pow(rand.UNorm(), pow));
+
+    public static float RangeXYPow(ref this Random rand, float3 minmaxpow)
+      => math.lerp(minmaxpow.x, minmaxpow.y, math.pow(rand.UNorm(), minmaxpow.z));
+
+    public static float2 RangeXYPow2(ref this Random rand, float3 minmaxpow)
+      => math.lerp(minmaxpow.x, minmaxpow.y, math.pow(rand.UNorm2(), minmaxpow.z));
+
+    public static int RangeXY(ref this Random rand, int2 minmax)
+      => rand.NextInt(minmax.x, minmax.y);
 }
 
 } // namespace Sketch
